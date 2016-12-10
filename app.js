c007257921a36b16211ddcd0601bd61634d443b2
danielrussellLA <danielrussell16@gmail.com>
2016-12-10 14:21:13 -0800

    corrected hours before noon to display proper time

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// var currentDate = new Date();
-// var hours = currentDate.getHours();
-// var minutes = currentDate.getMinutes();
-// var seconds = currentDate.getSeconds();
-// var currentTime = hours + ':' + minutes + ':' + seconds;
-//
 var clock = document.getElementById('clock');
 
 setInterval(function(){
@@ -13,9 +7,9 @@ setInterval(function(){
 function getCurrentTime() {
   var currentDate = new Date();
   var hours = currentDate.getHours() > 12 ? currentDate.getHours() - 12 : currentDate.getHours();
+  hours === 0 ? hours = 12 : hours = hours;
   var minutes = currentDate.getMinutes();
   var seconds = currentDate.getSeconds() < 10 ? '0' + currentDate.getSeconds() : currentDate.getSeconds();
-  // var milliseconds = currentDate.getMilliseconds();
   var currentTime = hours + ':' + minutes + ':' + seconds;
   return currentTime;
 }
